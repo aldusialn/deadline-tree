1651e55ae3274c842edbb7fbdefacb859cfb02fa
Aldiyar Alen <aldiyar.alen@gmail.com>
2026-02-16 07:59:23 -0500

    Adds clerk auth instead of nextauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 import Image from "next/image";
-import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation"
-import { authOptions} from '@/app/api/auth/[...nextauth]/route'
+
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
-
-  if(!session) {
-    redirect('/login')
-  }
   
   return (
    <div>
-    <h1>Welcome {session.user?.name}</h1>
+
 
    </div>
   );
